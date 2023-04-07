e1191fbde42a8cb7ecc400b26f898f8cdc21a4c6
Ekeh Divine <divineekeh6@gmail.com>
2023-04-07 16:44:46 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
  * @s: Input String
  * Return: the lenght of string
  */
-_strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (s[0] != '\0')
 		return (1 + _strlen_recursion(s + 1));
