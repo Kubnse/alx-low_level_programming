5f367b07b505665b8f1d3063623275359930b7b3
Ekeh Divine <divineekeh6@gmail.com>
2023-05-11 07:06:57 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * create_file - a file with read/write access for user
  * @filename: file to create
@@ -7,24 +12,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rstatus, i;
+	int fdo, fdw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fdo < 0)
 		return (-1);
 
-	if (text_content)
-	{
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
-		rstatus = write(fd, text_content, i);
-		if (rstatus == -1)
-			return (-1);
-	}
+	while (text_content && *(text_content + len))
+		len++;
 
-	close(fd);
+	fdw = write(fdo, text_content, len);
+	close(fdo);
+	if (fdw < 0)
+		return (-1);
 	return (1);
 }
